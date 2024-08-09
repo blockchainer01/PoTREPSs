59e33c09eb5f675812e2ad67b810cc6e2df5318c
YeahPoT <155350484+blockchainer01@users.noreply.github.com>
2024-08-09 11:37:44 +0800

    Update Plot_attack_signal.m

--- Application1/Hardware/Plot/Plot_attack_signal.m
+++ Application1/Hardware/Plot/Plot_attack_signal.m
@@ -5,7 +5,6 @@ Ts=1e-4;
 tmax=20/Ts;  % run time
 t_droop=5/Ts; % droop time
 Steps_con=500; 
-%% 1.使用求解器的情况
 
 Au=0.1; % magnitude of the attack on the control signal
 Omega=0.1; % angular frequency
@@ -21,10 +20,10 @@ figure(6)
 set(groot,'defaultAxesTickLabelInterpreter','latex');  
 set(groot,'defaulttextinterpreter','latex');
 set(groot,'defaultLegendInterpreter','latex');
-height=700;%高度 62
-width=127/72*height;%宽度，像素数127
-left=100;%距屏幕左下角水平距离
-bottem=100;%距屏幕左下角垂直距离
+height=700;
+width=127/72*height;
+left=100;
+bottem=100;
 set(gcf,'position',[left,bottem,width,height])
 
 % pp(1)=stem(att_all','.','MarkerSize',2);
@@ -36,13 +35,13 @@ hold on
 pp(2)=stem((1:300)*15/300+5,att_u','MarkerSize',0.5);
 hold off
 
-% set(pp(1),'LineStyle','-'); %设置颜色和线宽
-% set(pp(2),'LineStyle','-.'); %设置颜色和线宽
+% set(pp(1),'LineStyle','-'); 
+% set(pp(2),'LineStyle','-.'); 
 co1=[0.4118    0.6706    0.6196];
 co2='b';
-set(pp,{'Color'},{co1;co2},{'LineStyle'},{'-';'-.'}); %设置颜色和线宽
+set(pp,{'Color'},{co1;co2},{'LineStyle'},{'-';'-.'}); 
 
-set(pp(1),'LineWidth',1) %set(h,{'LineWidth'},{2})==set(h,'LineWidth',2),灏嗘墍鏈夌嚎瀹借缃负2
+set(pp(1),'LineWidth',1) 
 set(pp(2),'LineWidth',3) 
 
 ylim([-0.12,0.11]);
